dbbe44e4ee4f790ea060717e780cd76484ee9e04
gustavoshr <gustavoriquesb2002@gmail.com>
2022-05-15 19:29:25 -0300

    math

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor máximo: " + Math.Max(5, 10));
+            Console.WriteLine("Valor mínimo: " + Math.Min(5, 10));
+
+            Console.WriteLine("Raiz quadrada: " + Math.Sqrt(64));
+            Console.WriteLine("Arredondamento: " + Math.Round(9.99));
+
+        }
+    }
+}
